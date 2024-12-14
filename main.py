e2c0f74cd8f20fc000fb5e41a1bfcb70d0048ccf
Victor Buava Leite <victor-leite@Victors-MacBook-Air.local>
2024-12-14 15:07:48 -0300

    feat: test

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/items")
+def read_items():
+    result = n + 1
+    return {"result": result}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
