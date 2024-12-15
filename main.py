83763e3dc1f64f98fdf5b6314f84b88b06b4bc7a
Victor Leite <victorbleite@outlook.com>
2024-12-15 19:30:32 -0300

    fix: resolve issue #1

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/items")
-def read_items():
-    result = n + 1
-    return {"result": result}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+from fastapi import FastAPI\n\napp = FastAPI()\n\n@app.get(\"/items\")\ndef read_items():\n    n = 10\n    result = n + 1\n    return {\"result\": result}\n\nif __name__ == \"__main__\":\n    import uvicorn\n    uvicorn.run(\"main:app\", host=\"127.0.0.1\", port=8000, reload=True)
