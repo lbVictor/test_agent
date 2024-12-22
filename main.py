1ab69de193de53efa2923357efe6597735d0b32d
Victor Leite <victorbleite@outlook.com>
2024-12-22 13:09:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/items")
 def read_items():
-    result = 1 + 1
+    result = n + 1
     return {"result": result}
-
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
