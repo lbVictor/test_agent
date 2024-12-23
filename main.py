e3b80ff1d8189e449be4afd7c1e58c4ac141ad73
Victor Leite <victorbleite@outlook.com>
2024-12-23 10:52:36 -0300

    Fix: Resolve issue #36 - TypeError: can only concatenate str (not \"int\") to str

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/items")
 def read_items():
-    n = "2"
+    n = 2
     result = n + 1
     return {"result": result}
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
